96eef639486796e8936e5d1230d213ac3e47813e
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-05-24 08:35:26 +0200

    changed numberToFraction method with fracty API

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,6 +2,7 @@ import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
 import { numberToFraction } from '../helpers.js';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -130,7 +131,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? numberToFraction(ing.quantity).toString() : ''
+            ing.quantity ? fracty(ing.quantity).toString() : ''
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
